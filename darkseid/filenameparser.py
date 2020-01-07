9fe26dde4b4ddfa9e91cb6aa3c93208220ff5abc
Brian Pepple <bdpepple@gmail.com>
2020-01-06 22:31:51 -0500

    Remove license text

--- darkseid/filenameparser.py
+++ darkseid/filenameparser.py
@@ -5,21 +5,6 @@ This should probably be re-written, but, well, it mostly works!
 
 # Copyright 2012-2014 Anthony Beville
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     http://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Some portions of this code were modified from pyComicMetaThis project
-# http://code.google.com/p/pycomicmetathis/
-
 import re
 import os
 from urllib.parse import unquote
