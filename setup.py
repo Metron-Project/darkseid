0e26a47cba0440ac5436ee0bf38b705ae6e6394c
Brian Pepple <bdpepple@gmail.com>
2020-01-07 00:29:18 -0500

    Don't install the test directory

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email="bdpepple@gmail.com",
     url="https://github.com/bpepple/darkseid",
     license="GPLv3",
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     python_requires=">=3.6",
     install_requires=["natsort", "pillow"],
     classifiers=[
