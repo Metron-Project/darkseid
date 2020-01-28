46f3c4b414da07d984ce4e4221946be587d71866
Brian Pepple <bdpepple@gmail.com>
2020-01-28 14:27:07 -0500

    Bump doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Brian Pepple'
 author = 'Brian Pepple'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
