99cafe983779e0985d4ecff0b2ea6aa586745b99
Brian Pepple <bdpepple@gmail.com>
2020-06-29 12:32:47 -0400

    Refactor issuestring tests

--- tests/test_darkseid_issuestring.py
+++ tests/test_darkseid_issuestring.py
@@ -1,49 +1,51 @@
-from unittest import TestCase, main
-
 from darkseid.issuestring import IssueString
 
 
-class TestIssueString(TestCase):
-    def test_issue_string_pad(self):
-        val = IssueString(int(1)).as_string(pad=3)
-        self.assertEqual(val, "001")
-
-    def test_issue_float(self):
-        val = IssueString("1½").as_float()
-        self.assertEqual(val, 1.5)
-
-    def test_issue_float_half(self):
-        val = IssueString("½").as_float()
-        self.assertEqual(val, 0.5)
-
-    def test_issue_verify_float(self):
-        val = IssueString("1.5").as_float()
-        self.assertEqual(val, 1.5)
-
-    def test_issue_string_no_value_as_int(self):
-        val = IssueString("").as_int()
-        self.assertIsNone(val)
-
-    def test_issue_int(self):
-        val = IssueString("1").as_int()
-        self.assertEqual(val, 1)
-
-    def test_issue_float_as_int(self):
-        val = IssueString("1.5").as_int()
-        self.assertEqual(val, 1)
-
-    def test_issue_string_monsters_unleashed(self):
-        val = IssueString("1.MU").as_string(3)
-        self.assertEqual(val, "001.MU")
-
-    def test_issue_string_minus_one(self):
-        val = IssueString("-1").as_string(3)
-        self.assertEqual(val, "-001")
-
-    def test_issue_string_none_value(self):
-        val = IssueString("Test").as_string()
-        self.assertEqual(val, "Test")
+def test_issue_string_pad():
+    val = IssueString(int(1)).as_string(pad=3)
+    assert val == "001"
 
 
-if __name__ == "__main__":
-    main()
+def test_issue_float():
+    val = IssueString("1½").as_float()
+    assert val == 1.5
+
+
+def test_issue_float_half():
+    val = IssueString("½").as_float()
+    assert val == 0.5
+
+
+def test_issue_verify_float():
+    val = IssueString("1.5").as_float()
+    assert val == 1.5
+
+
+def test_issue_string_no_value_as_int():
+    val = IssueString("").as_int()
+    assert val is None
+
+
+def test_issue_int():
+    val = IssueString("1").as_int()
+    assert val == 1
+
+
+def test_issue_float_as_int():
+    val = IssueString("1.5").as_int()
+    assert val == 1
+
+
+def test_issue_string_monsters_unleashed():
+    val = IssueString("1.MU").as_string(3)
+    assert val == "001.MU"
+
+
+def test_issue_string_minus_one():
+    val = IssueString("-1").as_string(3)
+    assert val == "-001"
+
+
+def test_issue_string_none_value():
+    val = IssueString("Test").as_string()
+    assert val == "Test"
