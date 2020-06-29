88600c6c80ea1a9e940ef7e9d98c0fbabd09db45
Brian Pepple <bdpepple@gmail.com>
2020-06-29 12:26:52 -0400

    Rewrite utils for pytest

--- tests/test_darkseid_utils.py
+++ tests/test_darkseid_utils.py
@@ -1,68 +1,61 @@
-import tempfile
-import unittest
 from pathlib import Path
 
 from darkseid import utils
 
 
-class TestUtils(unittest.TestCase):
-    def setUp(self):
-        self.tmp_dir = tempfile.TemporaryDirectory()
-        self.tmp_file_1 = tempfile.NamedTemporaryFile(
-            suffix=".cbz", dir=self.tmp_dir.name
-        )
-        self.tmp_file_2 = tempfile.NamedTemporaryFile(
-            suffix=".cbz", dir=self.tmp_dir.name
-        )
-
-    def tearDown(self):
-        self.tmp_file_1.close()
-        self.tmp_file_2.close()
-        self.tmp_dir.cleanup()
-
-    def test_remove_articles(self):
-        txt = "The Champions & Inhumans"
-        new_txt = utils.remove_articles(txt)
-        self.assertEqual(new_txt, "champions inhumans")
-
-    def test_list_to_string(self):
-        thislist = ["apple", "banana", "cherry"]
-        expected_result = "apple; banana; cherry"
-
-        list_string = utils.list_to_string(thislist)
-        self.assertEqual(list_string, expected_result)
-
-    def test_unique_name(self):
-        new_file = self.tmp_file_1.name
-        new_name = utils.unique_file(new_file)
-        # Now let's create our expected result
-        path = Path(self.tmp_file_1.name)
-        result = Path(path.parent).joinpath(f"{path.stem} (1){path.suffix}")
-
-        self.assertEqual(new_name, result)
-
-    def test_recursive_list_with_file(self):
-        expected_result = []
-        expected_result.append(Path(self.tmp_file_1.name))
-
-        file_list = []
-        file_list.append(self.tmp_file_1.name)
-        result = utils.get_recursive_filelist(file_list)
-
-        self.assertEqual(result, expected_result)
-
-    def test_recursive_list_with_directory(self):
-        expected_result = []
-        expected_result.append(Path(self.tmp_file_2.name))
-        expected_result.append(Path(self.tmp_file_1.name))
-        expected_result = sorted(expected_result)
-
-        file_list = []
-        file_list.append(self.tmp_dir.name)
-        result = utils.get_recursive_filelist(file_list)
-
-        self.assertEqual(result, expected_result)
+def test_remove_articles():
+    txt = "The Champions & Inhumans"
+    expected_result = "champions inhumans"
+    result = utils.remove_articles(txt)
+    assert result == expected_result
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_list_to_string():
+    thislist = ["apple", "banana", "cherry"]
+    expected_result = "apple; banana; cherry"
+
+    result = utils.list_to_string(thislist)
+    assert result == expected_result
+
+
+def test_unique_name(tmp_path):
+    new_file = tmp_path
+    result = utils.unique_file(new_file)
+    expected_result = Path(new_file.parent / f"{new_file.stem} (1){new_file.suffix}")
+
+    assert result == expected_result
+
+
+def test_recursive_list_with_file(tmp_path):
+    temp_file = tmp_path / "test.cbz"
+    expected_result = []
+    expected_result.append(temp_file)
+
+    file_list = []
+    file_list.append(temp_file)
+    result = utils.get_recursive_filelist(file_list)
+
+    assert result == expected_result
+
+
+def test_recursive_list_with_directory(tmp_path):
+    temp_dir = tmp_path / "recursive"
+    temp_dir.mkdir()
+
+    # Not really zip files, but we really only care about the file extension
+    temp_file_1 = temp_dir / "test-1.cbz"
+    temp_file_1.write_text("content")
+
+    temp_file_2 = temp_dir / "test-2.cbz"
+    temp_file_2.write_text("content")
+
+    expected_result = []
+    expected_result.append(temp_file_2)
+    expected_result.append(temp_file_1)
+    expected_result = sorted(expected_result)
+
+    file_list = []
+    file_list.append(temp_dir)
+    result = utils.get_recursive_filelist(file_list)
+
+    assert result == expected_result
