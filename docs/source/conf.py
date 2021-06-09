a49c18b04255392bbd793dc6b4a7d9bd1f2a319e
Brian Pepple <bdpepple@gmail.com>
2021-06-08 21:46:57 -0400

    Update docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Darkseid'
-copyright = '2020, Brian Pepple'
-author = 'Brian Pepple'
+project = "Darkseid"
+copyright = "2021, Brian Pepple"
+author = "Brian Pepple"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = "1.0.6"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,10 +31,10 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
