dbbbe6f1004b5d5216bdd76e51ea45aa6ffbf2fa
Brian Pepple <bdpepple@gmail.com>
2022-06-27 08:26:01 -0400

    Bump for for yet *another* release

--- darkseid/__init__.py
+++ darkseid/__init__.py
@@ -1,2 +1,2 @@
 """Projects version"""
-__version__ = "1.2.5"
+__version__ = "1.2.6"
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Brian Pepple"
 author = "Brian Pepple"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.5"
+release = "1.2.6"
 
 
 # -- General configuration ---------------------------------------------------
