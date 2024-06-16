0c771eb1848afe24c7b56b2ca591b9275621b813
Brian Pepple <bdpepple@gmail.com>
2024-06-16 12:00:45 -0400

    Add docstrings

--- darkseid/utils.py
+++ darkseid/utils.py
@@ -7,14 +7,19 @@ from collections import defaultdict
 from pathlib import Path
 
 
-def get_recursive_filelist(pathlist: list[Path]) -> list[Path]:
-    """Returns a path list of comic archives.
-
-    :param pathlist: A list of path objects
-    :type pathlist:  list of Path
+def get_recursive_filelist(path_list: list[Path]) -> list[Path]:
     """
+    Retrieves a list of files recursively from the provided paths.
+
+    Args:
+        path_list (list[Path]): List of paths to search for files.
+
+    Returns:
+        list[Path]: A sorted list of files found in the provided paths.
+    """
+
     filelist: list[Path] = []
-    for path_str in pathlist:
+    for path_str in path_list:
         path = Path(path_str)
         if path.is_dir():
             for comic_format in ["*.cbz", "*.cbr"]:
@@ -26,21 +31,30 @@ def get_recursive_filelist(pathlist: list[Path]) -> list[Path]:
 
 
 def list_to_string(list_of_strings: list[str]) -> str:
-    """Function that takes a list of string and converts it to a string.
-    For example: ["apple", "banana", "cherry, inc"] is changed
-    to 'apple; banana; "cherry, inc"'.
-
-    :param list_of_strings: A list of strings.
-    :type list_of_strings: list of str
     """
+    Converts a list of strings into a single comma-separated string.
+
+    Args:
+        list_of_strings (list[str]): The list of strings to convert.
+
+    Returns:
+        str: The comma-separated string.
+    """
+
     return ", ".join((f'"{item}"' if "," in item else item) for item in list_of_strings)
 
 
 def remove_articles(text: str) -> str:
-    """Takes a string and removes any articles in it.
-
-    :param str text: A string with articles (ex. 'and', 'a', 'the').
     """
+    Removes common articles from the input text.
+
+    Args:
+        text (str): The text from which articles are to be removed.
+
+    Returns:
+        str: The text with articles removed.
+    """
+
     articles = frozenset(
         {
             "&",
@@ -74,10 +88,16 @@ def remove_articles(text: str) -> str:
 
 
 def unique_file(file_name: Path) -> Path:
-    """Takes a filename and if one already exist with that name, returns a new filename.
-
-    :param Path file_name: A path objects
     """
+    Generates a unique file name by appending a number in parentheses if the original file name already exists.
+
+    Args:
+        file_name (Path): The original file name to make unique.
+
+    Returns:
+        Path: The unique file name.
+    """
+
     original_stem = file_name.stem
     for i in itertools.count(1):  # noqa: RET503
         if not file_name.exists():
@@ -86,6 +106,17 @@ def unique_file(file_name: Path) -> Path:
 
 
 def xlate(data: int | str | None, is_int: bool = False) -> int | str | None:
+    """
+    Translates data to an integer or string based on the provided flag.
+
+    Args:
+        data (int | str | None): The data to translate.
+        is_int (bool): A flag indicating whether to translate to an integer.
+
+    Returns:
+        int | str | None: The translated data.
+    """
+
     if data is None or data == "":
         return None
     if is_int:
